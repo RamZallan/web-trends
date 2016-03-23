875eabb962b51d99d727f4a6fa456043ec664c3b
Ram Zallan <ram.zallan@ht-la.org>
2016-03-22 17:27:19 -0700

    Adder site navigation.

--- 1996/js/scripts.js
+++ 1996/js/scripts.js
@@ -1,7 +1,14 @@
 /*jslint browser: true*/
 /*global $, jQuery*/
 /*jslint node: true */
+$(document).ready(function () {
+    $('.numbers').hover(function () {
+        $(this).toggleClass('spinning');
+    });
 
-$('.numbers').hover(function () {
-  $(this).toggleClass('spinning');
+    $('li').hover(function () {
+        $(this).css("opacity", "0.8");
+    }, function () {
+        $(this).css("opacity", "1");
+    });
 });
