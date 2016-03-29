10d3f4adacb18e1924374574366b72496e539a4a
Ram Zallan <Ram.Zallan@hthla.org>
2016-03-29 14:28:37 -0700

    Added 2000 site with placeholder text.

---
+++ 2000/js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $('#more').click(function () {
+        var el = $('.content'),
+            curHeight = $('.content').height(),
+            autoHeight = $('.content').css('height', 'auto').height();
+        $('.content').height(curHeight).animate({
+            height: autoHeight == curHeight ? "240px" : autoHeight
+        }, 1000);
+        $("#more").text("Toggle Text");
+    });
+});
